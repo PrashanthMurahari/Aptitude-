7ac497d5765297301ebf02fc37135a1692fde33b
Prashanth Murahari <murahariprashanth1@gmail.com>
2023-03-07 09:50:39 +0530

    few changes

--- PermutationsandCombinations/Arrange_the_letters.py
+++ PermutationsandCombinations/Arrange_the_letters.py
@@ -1,12 +1,10 @@
 def finding_number_of_ways_arranging(given_word, length_of_the_word, list_of_removing_similar_letters):
     print("Number of ways of arranging the word  = " + str(length_of_the_word) + "!")
 
-    number_of_arranging_all_letters = 1
-    for factorial_of_arranging_all_letters in range(1, length_of_the_word + 1):
-        number_of_arranging_all_letters *= factorial_of_arranging_all_letters
+    number_of_arranging_all_letters=math.factorial(length_of_the_word)
     print("Number of ways of arranging the word : " + str(number_of_arranging_all_letters))
 
-    similar_letters_factorial = 1
+    total_similar_letters_factorial = 1
     for similar_letters in list_of_removing_similar_letters:
         counting_the_factorial_of_similar_letters = 0
         for same_letters_in_given_word in given_word:
@@ -15,17 +13,16 @@ def finding_number_of_ways_arranging(given_word, length_of_the_word, list_of_rem
 
         if counting_the_factorial_of_similar_letters > 1:
             print("Similar Letters : " + str(counting_the_factorial_of_similar_letters) + " " + similar_letters + "'s = " + str(counting_the_factorial_of_similar_letters) + "!")
-        for factorial in range(1, counting_the_factorial_of_similar_letters + 1):
-            similar_letters_factorial *= factorial
-
-    if similar_letters_factorial > 1:
-        print("Arranging the similar letters : " + str(similar_letters_factorial))
-        number_of_ways_of_arranging_the_given_word = number_of_arranging_all_letters / similar_letters_factorial
-        print("Total number of ways of arranging the word = " + str(number_of_arranging_all_letters) + "/" + str(similar_letters_factorial))
+        similar_letters_factorial=math.factorial(counting_the_factorial_of_similar_letters)
+        total_similar_letters_factorial*=similar_letters_factorial
+    if total_similar_letters_factorial > 1:
+        print("Arranging the similar letters : " + str(total_similar_letters_factorial))
+        number_of_ways_of_arranging_the_given_word = number_of_arranging_all_letters / total_similar_letters_factorial
+        print("Total number of ways of arranging the word = " + str(number_of_arranging_all_letters) + "/" + str(total_similar_letters_factorial))
         print("Total number of ways of arranging the word : " + str(int(number_of_ways_of_arranging_the_given_word)))
     else:
         pass
-
+import math
 given_word = input().lower()
 condition = input().lower()
 length_of_the_word = len(given_word)
